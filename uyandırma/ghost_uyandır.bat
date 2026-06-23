adc69b140d1ab0b34cb03e05649f026da412a4d9
Musab Duman <dum4n2542@gmail.com>
2026-06-23 20:48:29 +0300

    öylesine

--- uyandırma/ghost_uyandır.bat
+++ uyandırma/ghost_uyandır.bat
@@ -1,7 +1,5 @@
 @echo off
-title Ghost Denetci Calisiyor
 :: Projenin olduğu klasöre git
 cd /d "C:\Users\dum4n\Desktop\vs.code\asistan"
-:: Dinleyinci kodunu çalıştır
-python dinleyici.pyw
-pause
+start pythonw dinleyici.pyw
+exit
